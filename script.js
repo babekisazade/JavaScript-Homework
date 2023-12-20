ce5af0b55d675978e3b9107d194cb6db2f656573
babekisazade <babekisazade153@gmail.com>
2023-12-20 12:21:56 +0400

    Java Script Homework

---
+++ script.js
@@ -0,0 +1,213 @@
+// alert (Salam Dünya)
+
+alert("Hello World");
+
+//Task-01
+console.log("Bu konsola verilen mesajdir.");
+
+// Task-02
+// Adı name və adınızın mənimsədildiyi sabit dəyişən təyin edin.
+//Bu dəyişənin dəyərini konsolda göstərin.
+
+ // let name = "Babek";
+ // const userName = "Babek Isazade";
+ // console.log("ad:", name);
+
+// Task-03
+// Bu adlardan hansının dəyişən üçün istifadə oluna biləcəyini göstərin.
+// Birdən çox variant seçin.
+
+
+// 2user -- Olmaz
+// c@rt -- Olmaz
+// person -- Olar
+// title color -- Olmaz
+// console -- Olmaz
+// $add -- Olar
+
+// Task-04
+// Depodaki bütün kitabların ümumi dəyərini konsola çıxarın
+// Kitab mağazasındaki kitabın qiyməti bookPrice dəyişənində
+// və depodaki kitab sayı bookAmount dəyişənində saxlanılır.
+// Depodaki bütün kitabların ümumi dəyərini konsola çıxarın.
+// =>let bookPrice = 750;
+// =>let bookAmount = 14;
+
+ // let bookPrice = 750
+ // let bookAmount = 14
+ // let allAmount = bookPrice * bookAmount
+ // console.log("Magazadaki bir kitabin qiymeti : ", bookPrice);
+ // console.log("Depodaki kitablarin sayi :", bookPrice);
+ // console.log("Depodaki butun kitablarin umumilikde deyeri :", allAmount);
+
+// Task-05
+// Dəyişən dəyərini saniyə metrə çevirin və onu konsola çıxarın.
+// kmhSpeed ​​dəyişəni avtomobilin sürətinin «saatda kilometrlərlə»
+// dəyərini saxlayır. msSpeed ​​dəyişəni təyin edin və
+// ona eyni sürət dəyərini «saniyədə metrə» çevirərək yazın.
+// msSpeed dəyişəninin dəyərini konsola çıxarın.
+// =>let kmhSpeed = 75;
+
+// Task-06
+// İstifadəçinin doğum tarixini soruşan və bu günə qədər yaşadığı
+// günlərin sayını göstərən proqram yazın.
+
+// const userBirthDate = +promt('Enter your birth date');
+// const daysLived = userBirthDate * 365
+// alert(daysLived)
+
+// Task-07
+// Ədədlər üzərində əməliyyatları yerinə yetirin.
+// num dəyişənində iki rəqəmli ədəd saxlanılır.
+// Konsola çıxaran belə proqram yazın:
+// - Bu ədədin beşinci dərəcə qüvvətə qaldırılmış vəziyyətini.
+// - 3-ə bölünmədən qalan bu ədədin qalığını.
+
+// Task-08
+// Konsola bu növ mesaj çıxarın “Sizin adınız [firstName] [lastName].
+// Tanışlığımıza çox şadam!”
+// firstName dəyişənində istifadəçinin adı,
+// lastName — dəyişənində isə soyadı saxlanır.
+// Konsola bu növ mesaj çıxarın “Sizin adınız [firstName] [lastName]. Tanışlığımıza çox şadam!”
+// =>let firstName = 'Alan';
+// =>let lastName = 'Turing';
+
+// Task-09
+// Şablon mətnlərdən istifadə edərək,
+// konsola “Sizi bir daha görməyimizə şadıq, [firstName] [lastName]”.
+// firstName dəyişənində istifadəçinin adı, lastName — dəyişənində isə soyadı saxlanır.
+
+// Task-10
+// Addaki hərflərin sayını konsola çıxarın.
+// fullName dəyişənində istifadəçinin tam adı saxlanılır. Addaki hərflərin sayını konsola çıxarın.
+// Probel hərf olaraq sayılmır.
+// let fullName = "Murad Orucov"
+
+
+// Task-11
+// İstifadəçinin ad soyadının baş hərflərini konsola çıxaran proqram yazın.
+// İstifadəçinin adını və soyadını boşluqlarla ayıraraq fullName dəyişəninə yazın.
+// fullName dəyişəninin dəyərindən istifadə edərək istifadəçinin baş hərflərini
+// konsola çıxaran proqram yazın.
+
+// Task-12
+// İstifadəçidən tam adını daxil etməyini istəyin və soyadı ilə adının yerini dəyişin.
+//  tam adı «AD SOYAD» formatında daxil etməyi xahiş edin.
+// Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,
+
+// Task-13
+// Konsola "Salam, [ad]!" formatında salamlama mesajı çıxarın.
+// İstifadəçidən adını daxil etməsini istəyin.
+// Konsola "Salam, [ad]!" formatında salamlama mesajı çıxarın.
+
+ // let fullName = promt("Write your name..");
+// console.log("Salam, ", fullName);
+
+// Task-14
+// İki ədədin toplama, çıxma, vurma və bölməsinin nəticələrin konsola alt-alta 4 sətir olaraq çıxarın.
+// İstifadəçidən iki ayrı prompt əmri ilə iki ədəd daxil etməsini istəyin.
+// Bu ədədlərin toplama, çıxma, vurma və bölməsinin nəticələrin konsola
+// alt-alta 4 sətir olaraq çıxarın.
+
+
+// Task-15
+// 12345 və 98765 ədədlərinin hasilindən alınan nəticənin 6-cı rəqəmini tapan proqram yazın
+
+// Task-16
+// Əməliyyatların nəticəsini müəyyənləşdirin.
+
+    // "" + 1 + 0 =
+
+    // "" - 1 + 0 = -1
+
+    // "true" - false =
+
+    // 6 / "3" =
+
+    // 4 + 5 + "px" =
+
+    // "$" + 4 +5 =  
+
+    // "4" - 2 =
+
+    // "4px" - 2 + "px" =
+
+    // 7 / 0 =
+
+    // "-9" + 5 =
+
+    // "-9" - 5 =
+
+    // null + 1 =
+
+    // undefined + 1 =
+
+
+// Task-17
+// Rəqəmlərin cəmini göstərən proqram yazın.
+// İstifadəçidən dörd rəqəmli ədəd daxil etməsini xahiş edən və
+// sonra daxil edilmiş ədədlərin rəqəmlərinin cəmini konsola çıxaran proqram yazın.
+
+// Task-18
+// İstifadəçinin soyadını və adını almaq üçün proqram tərtib edin.
+// Proqram bir mesajla istifadəçinin soyadını və adını daxil etməsini istəyir və
+// konsola «Salam, <ad>!» mesajını çıxarır.
+
+// Task-19
+// Qiyməti mətn halına salan proqram tərtib edin.
+// İstifadəçidən «530.90» formatında qiymət daxil etməyi xahiş edən və
+// qiyməti «530 manat 90 qəpik» formatında konsola çıxaran proqram tərtib edin.
+
+// Task-20
+// Vergi nəzərə alınmadan əmək haqqı məbləğini hesablayan proqram tərtib edin.
+// Proqram istifadəçidən əmək haqqı məbləğin soruşur və
+// 13% vergi istisna olmaqla məbləği konsola çıxarır.
+
+// Task-21
+// İstifadəçidən soyad, ad və ata adını istəyən,
+// soyadı və baş hərfləri konsola çıxaran proqram tərtib edin.
+//Məsələn, «İvanov Pyotr Sergeevich» — «İvanov P.S.».
+
+// Task-22
+// Depozitdən mənfəətin hesablanması üçün proqram yazın.
+// Proqram istifadəçidən əmanətin məbləğini, müddətini və
+// faiz dərəcəsini daxil etməsini istəyir və gözlənilən mənfəəti hesablayır.
+
+// Task-23
+// Müxtəlif müqayisə operatorları ilə 5 ifadə yazın.
+// value dəyişəni üçün müxtəlif müqayisə operatorları ilə 5 ifadə yazın,
+// hansıki əməliyyat nəticəsi value = 10 üçün true və value = 20 üçün false olacaq.
+
+// Task-24
+// İstifadəçidən yaşını soruşun.
+// Əgər yaşı 18-dən azdırsa, o zaman brauzerdə «Access denied» bildirişi çıxarın.
+// Əks halda «Access granted» bildirişini çıxarın.
+
+// Task-25
+// Üç ədədin ortancılını tapmaq üçün proqram yazın.
+// İstifadəçidən bir bildirişlə üç ikirəqəmli ədəd daxil etməsini xahiş edin.
+// Konsola orta ədədi, yəni nə ən böyüyü, nə də ən kiçiyini çıxarmayın.
+// İstifadəçi bir neçə eyni nömrə daxil edibsə, xəta bildirişi çıxarın.
+// "45 46 47"
+// "47 46 45"
+// "46 45 47"
+// "46 47 45"
+
+// Task-26
+// İstifadəçidən bir il daxil etməsini soruşun və ilin uzun il olub olmadığını yoxlayın.
+// Uzun il 4-ə qalıqsız bölünən ildir.
+// Daxil edilmiş il uzun ildirsə, konsola mesaj cıxarın.
+// «İl uzun il deyil» və ya «İl uzun ildir»mesajını çıxarın.
+
+
+
+
+
+
+
+
+
+
+
+
+
